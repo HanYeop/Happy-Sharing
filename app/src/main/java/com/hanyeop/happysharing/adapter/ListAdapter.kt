5d38474a74b75f571eeffa9bd99f84b46035b22c
Hanyeop <super7615@naver.com>
2021-09-06 23:54:53 +0900

    리스트 디자인 임시로 구현

---
+++ app/src/main/java/com/hanyeop/happysharing/adapter/ListAdapter.kt
@@ -0,0 +1,45 @@
+package com.hanyeop.happysharing.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hanyeop.happysharing.databinding.ItemObjectBinding
+
+class ListAdapter(val nameList : Array<String>, val desList : Array<String>)
+    : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+
+    // 생성된 뷰 홀더에 값 지정 ( onBindViewHolder 에서 해도 됨. )
+    class ListViewHolder(val binding: ItemObjectBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(name: String, des: String) {
+            // 뷰 홀더의 제목과 설명
+            binding.nameText.text = name
+            binding.descriptionText.text = des
+
+//            // 뷰 홀더 클릭시 디테일뷰로
+//            binding.cardView.setOnClickListener {
+//                val intent: Intent = Intent(it.context, DetailViewActivity::class.java)
+//                intent.putExtra("currentName", name)
+//                intent.putExtra("currentDes", des)
+//                it.context.startActivity(intent)
+//            }
+        }
+    }
+
+    // 어떤 xml 으로 뷰 홀더를 생성할지 지정
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemObjectBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ListViewHolder(binding)
+    }
+
+    // 뷰 홀더에 데이터 바인딩
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(nameList[position],desList[position])
+    }
+
+    // 뷰 홀더의 개수 리턴
+    override fun getItemCount(): Int {
+        return nameList.size
+    }
+}
--- app/src/main/java/com/hanyeop/happysharing/fragment/ListFragment.kt
+++ app/src/main/java/com/hanyeop/happysharing/fragment/ListFragment.kt
@@ -1,9 +1,38 @@
 package com.hanyeop.happysharing.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.hanyeop.happysharing.R
+import com.hanyeop.happysharing.adapter.ListAdapter
+import com.hanyeop.happysharing.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment(R.layout.fragment_list) {
 
+    // 참조 관리
+    private var _binding : FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var nameList : Array<String>
+    lateinit var descriptionList : Array<String>
+
+    // ListAdapter 선언
+    private lateinit var listAdapter: ListAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 뷰바인딩
+        _binding = FragmentListBinding.bind(view)
+
+        // 이름과 설명 초기화
+        nameList = resources.getStringArray(R.array.item_name)
+        descriptionList = resources.getStringArray(R.array.item_description)
+
+        binding.apply {
+            listAdapter = ListAdapter(nameList,descriptionList)
+            recyclerView.adapter = listAdapter
+        }
+    }
 }
