8eb7d2d5586ae6b099ef041a5a4da1de1f0b1b64
Hanyeop <super7615@naver.com>
2021-09-16 02:15:24 +0900

    프로필 사진 올리기 구조 수정, 로딩 다이얼로그 추가

--- app/src/main/java/com/hanyeop/happysharing/ProfileActivity.kt
+++ app/src/main/java/com/hanyeop/happysharing/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package com.hanyeop.happysharing
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +19,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import com.hanyeop.happysharing.databinding.ActivityMainBinding
 import com.hanyeop.happysharing.databinding.ActivityProfileBinding
+import com.hanyeop.happysharing.dialog.LoadingDialog
 import com.hanyeop.happysharing.model.UserDTO
 import com.hanyeop.happysharing.util.Constants.Companion.PICK_PROFILE_FROM_ALBUM
 import com.hanyeop.happysharing.util.Constants.Companion.TAG
@@ -29,6 +32,11 @@ class ProfileActivity : AppCompatActivity() {
 
     private var uId : String? = null
     private var userDTO = UserDTO()
+
+    private var imageUri : Uri? = null
+    private lateinit var dialog : Dialog
+
+    private var delete = false
 
     // 뷰모델 연결
     private val firebaseViewModel : FirebaseViewModel by viewModels()
@@ -56,6 +64,8 @@ class ProfileActivity : AppCompatActivity() {
             }
         })
 
+        dialog = LoadingDialog(this@ProfileActivity)
+
         // 프로필 편집
         binding.apply {
 
@@ -66,9 +76,15 @@ class ProfileActivity : AppCompatActivity() {
                 this@ProfileActivity.startActivityForResult(photoPickerIntent,PICK_PROFILE_FROM_ALBUM)
             }
 
-            // TODO 프로필 초기화
+            // 프로필 이미지 초기화
             profileResetButton.setOnClickListener {
+                userDTO.imageUri = "default"
+                Glide.with(this@ProfileActivity).load("")
+                    .placeholder(R.drawable.ic_baseline_person_24)
+                    .apply(RequestOptions().circleCrop()).into(binding.profileImageView)
 
+                // 삭제
+                delete = true
             }
 
             // 취소 버튼
@@ -80,45 +96,74 @@ class ProfileActivity : AppCompatActivity() {
             okButton.setOnClickListener {
                 userDTO.userId = userIdEditView.text.toString() // 입력한 닉네임으로 변경
 
-                firebaseViewModel.profileEdit(userDTO)
-                Toast.makeText(this@ProfileActivity,"변경 완료",Toast.LENGTH_SHORT).show()
-
-                finish()
+                when {
+                    // 프로필 이미지 초기화 했을 때
+                    delete -> {
+                        dialog.show()
+                        deleteImage()
+                    }
+                    // 이미지를 변경하지 않았을 때
+                    imageUri == null -> {
+                        firebaseViewModel.profileEdit(userDTO)
+                        finish()
+                    }
+                    // 이미지를 변경했을 때
+                    imageUri != null -> {
+                        dialog.show()
+                        uploadImage()
+                    }
+                }
             }
         }
+    }
+
+    // 이미지 업로드
+    private fun uploadImage() {
+        var storageRef =
+            FirebaseStorage.getInstance().reference.child("userProfileImages").child(uId!!)
+        storageRef.putFile(imageUri!!).continueWithTask { _: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }.addOnSuccessListener { uri ->
+            // 이미지가 업로드 되면 불러오고 종료
+            userDTO.imageUri = uri.toString()
+            firebaseViewModel.profileEdit(userDTO)
+            Toast.makeText(this@ProfileActivity,"변경 완료",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+            finish()
+        }
+    }
+
+    // 이미지 삭제
+    private fun deleteImage(){
+        FirebaseStorage.getInstance().reference.child("userProfileImages").child(uId!!).delete()
+            .addOnSuccessListener {
+                // 이미지가 삭제되면 종료
+                firebaseViewModel.profileEdit(userDTO)
+                Toast.makeText(this@ProfileActivity,"변경 완료",Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@ProfileActivity,"변경 완료",Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+                finish()
+            }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // 프로필 사진 이미지 파이어베이스에 올리기
+        // 프로필 사진 이미지 미리 보여주기
         if(requestCode == PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK){
-            var imageUri = data?.data
+            delete = false
+
+            imageUri = data?.data!!
 
             Log.d(TAG, "onActivityResult: $imageUri")
 
             Glide.with(this@ProfileActivity).load(imageUri)
                 .placeholder(R.drawable.ic_baseline_person_24)
                 .apply(RequestOptions().circleCrop()).into(binding.profileImageView)
-
-            var storageRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(uId!!)
-            storageRef.putFile(imageUri!!).continueWithTask { _: Task<UploadTask.TaskSnapshot> ->
-                return@continueWithTask storageRef.downloadUrl
-            }.addOnSuccessListener { uri ->
-                userDTO.imageUri = uri.toString()
-
-                Log.d(TAG, "onActivityResult: ${uri.toString()}")
-
-                firebaseViewModel.profileEdit(userDTO)
-
-//                Glide.with(this@ProfileActivity).load(uri.toString())
-//                    .placeholder(R.drawable.ic_baseline_person_24)
-//                    .apply(RequestOptions().circleCrop()).into(binding.profileImageView)
-
-//                var map = HashMap<String,Any>()
-//                map["image"] = uri.toString()
-//                FirebaseFirestore.getInstance().collection("profileImages").document(uId!!).set(map)
             }
         }
     }
-}
---
+++ app/src/main/java/com/hanyeop/happysharing/dialog/LoadingDialog.kt
@@ -0,0 +1,23 @@
+package com.hanyeop.happysharing.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import com.hanyeop.happysharing.R
+
+class LoadingDialog(context: Context) : Dialog(context){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_loading)
+
+        // 취소 불가능
+        setCancelable(false)
+
+        // 배경 투명하게 바꿔줌
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+    }
+}
