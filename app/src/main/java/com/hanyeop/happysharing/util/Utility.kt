ad881db3ccda3846f04764784cea2b541b0c23da
Hanyeop <super7615@naver.com>
2021-09-21 15:33:13 +0900

    하루 전 시간 표시

--- app/src/main/java/com/hanyeop/happysharing/util/Utility.kt
+++ app/src/main/java/com/hanyeop/happysharing/util/Utility.kt
@@ -7,6 +7,16 @@ class Utility {
 
     companion object{
         fun timeConverter(time: Long) :String{
+            val currentTime = System.currentTimeMillis()
+            var diffTime = (currentTime - time) / 60000
+
+            if(diffTime < 60){
+                return "$diffTime 분 전"
+            }
+            else if(diffTime/60 < 24){
+                return "${diffTime/60} 시간 전"
+            }
+
             val simpleDataFormat = SimpleDateFormat("yyyy-MM-dd")
             return simpleDataFormat.format(time)
         }
