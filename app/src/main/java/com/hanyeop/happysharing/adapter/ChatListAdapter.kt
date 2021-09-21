0a036556e65f41aa8dcdf9f1544fac4c82ee1011
Hanyeop <super7615@naver.com>
2021-09-21 23:47:56 +0900

    채팅리스트 실시간 동기화

--- app/src/main/java/com/hanyeop/happysharing/adapter/ChatListAdapter.kt
+++ app/src/main/java/com/hanyeop/happysharing/adapter/ChatListAdapter.kt
@@ -12,12 +12,14 @@ import com.google.firebase.firestore.Query
 import com.hanyeop.happysharing.R
 import com.hanyeop.happysharing.databinding.ItemChatlistBinding
 import com.hanyeop.happysharing.model.ChatListDTO
+import com.hanyeop.happysharing.model.ItemDTO
 import com.hanyeop.happysharing.model.MessageDTO
 import com.hanyeop.happysharing.model.UserDTO
 import com.hanyeop.happysharing.util.Constants.Companion.TAG
 import com.hanyeop.happysharing.util.Utility
 
-class ChatListAdapter(private val uId: String)
+class ChatListAdapter(private val uId: String,
+                      private val listener : OnChatClickListener)
     : RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder>(){
 
     // 채팅방 리스트
@@ -27,21 +29,41 @@ class ChatListAdapter(private val uId: String)
     private val fireStore = FirebaseFirestore.getInstance()
 
     init {
-            fireStore.collection("chatList")
-                .whereArrayContains("chatPerson",uId)
-            .orderBy("timestamp", Query.Direction.DESCENDING)
-            .addSnapshotListener { querySnapshot, _ ->
+//            fireStore.collection("chatList")
+//                .whereArrayContains("chatPerson",uId)
+//            .orderBy("timestamp", Query.Direction.DESCENDING)
+//            .addSnapshotListener { querySnapshot, _ ->
+//
+//            if(querySnapshot == null) return@addSnapshotListener
+//
+//            // 문서 수신
+//            for (doc in querySnapshot.documentChanges) {
+//                // 문서가 추가될 경우 리사이클러 뷰에 추가
+//                if (doc.type == DocumentChange.Type.ADDED) {
+//                    Log.d(TAG, "$doc: ")
+//                    var message = doc.document.toObject(ChatListDTO::class.java)
+//                    chatList.add(message)
+//                }
+//            }
 
+        // 아이템 리스트 불러오기
+                fireStore.collection("chatList")
+//                    .whereArrayContains("chatPerson",uId)
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
+                    .addSnapshotListener { querySnapshot, _ ->
             if(querySnapshot == null) return@addSnapshotListener
 
-            // 문서 수신
-            for (doc in querySnapshot.documentChanges) {
-                // 문서가 추가될 경우 리사이클러 뷰에 추가
-                if (doc.type == DocumentChange.Type.ADDED) {
-                    Log.d(TAG, "$doc: ")
-                    var message = doc.document.toObject(ChatListDTO::class.java)
-                    chatList.add(message)
+            chatList.clear() // 리스트 초기화
+
+            // 리스트 불러오기
+            for(snapshot in querySnapshot.documents){
+                var item = snapshot.toObject(ChatListDTO::class.java)
+
+                if(item!!.chatPerson!!.contains(uId)) {
+                    chatList.add(item!!)
                 }
+
+                Log.d(TAG, "$item: ")
             }
 
             notifyDataSetChanged()
@@ -69,10 +91,10 @@ class ChatListAdapter(private val uId: String)
                                     .apply(RequestOptions().circleCrop())
                                     .into(profileImageView)
 
-//                                // 아이템 클릭 시
-//                                root.setOnClickListener {
-//                                    listener.onItemClick(item,userDTO)
-//                                }
+                                // 아이템 클릭 시
+                                root.setOnClickListener {
+                                    listener.onChatClick(message.toUid.toString())
+                                }
                             }
                         }
                 }
@@ -92,10 +114,10 @@ class ChatListAdapter(private val uId: String)
                                     .apply(RequestOptions().circleCrop())
                                     .into(profileImageView)
 
-//                                // 아이템 클릭 시
-//                                root.setOnClickListener {
-//                                    listener.onItemClick(item,userDTO)
-//                                }
+                                // 아이템 클릭 시
+                                root.setOnClickListener {
+                                    listener.onChatClick(message.fromUid.toString())
+                                }
                             }
                         }
                 }
@@ -104,6 +126,11 @@ class ChatListAdapter(private val uId: String)
                 timeText.text = Utility.chatTimeConverter(message.timestamp!!)
             }
         }
+    }
+
+    // 아이템 포지션 넘겨주기 위한 인터페이스
+    interface OnChatClickListener {
+        fun onChatClick(userUid : String)
     }
 
     override fun onCreateViewHolder(
--- app/src/main/java/com/hanyeop/happysharing/fragment/ChatFragment.kt
+++ app/src/main/java/com/hanyeop/happysharing/fragment/ChatFragment.kt
@@ -1,16 +1,18 @@
 package com.hanyeop.happysharing.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.hanyeop.happysharing.ChattingActivity
 import com.hanyeop.happysharing.R
 import com.hanyeop.happysharing.adapter.ChatListAdapter
 import com.hanyeop.happysharing.adapter.ListAdapter
 import com.hanyeop.happysharing.databinding.FragmentChatBinding
 import com.hanyeop.happysharing.databinding.FragmentListBinding
 
-class ChatFragment : Fragment(R.layout.fragment_chat) {
+class ChatFragment : Fragment(R.layout.fragment_chat), ChatListAdapter.OnChatClickListener {
     private var uId : String? = null
 
     // 참조 관리
@@ -31,15 +33,27 @@ class ChatFragment : Fragment(R.layout.fragment_chat) {
 
         binding.apply {
             // 리사이클러뷰 어댑터 연결
-            chatListAdapter = ChatListAdapter(uId.toString())
+            chatListAdapter = ChatListAdapter(uId.toString(),this@ChatFragment)
             chatListRecyclerView.adapter = chatListAdapter
         }
     }
 
+    // 채팅 클릭 시
+    override fun onChatClick(userUid: String) {
+        val intent = Intent(requireContext(), ChattingActivity::class.java)
+        intent.putExtra("otherUid",userUid)
+        startActivity(intent)
+    }
 
     // 프래그먼트는 뷰보다 오래 지속 . 프래그먼트의 onDestroyView() 메서드에서 결합 클래스 인스턴스 참조를 정리
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // 목록 갱신 위함
+    override fun onResume() {
+        super.onResume()
+        chatListAdapter.notifyDataSetChanged()
     }
 }
