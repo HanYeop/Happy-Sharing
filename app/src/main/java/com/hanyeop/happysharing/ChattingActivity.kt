d724715cdfa038ae00164cb02e25156ec7984216
Hanyeop <super7615@naver.com>
2021-09-21 21:35:35 +0900

    채팅 버그수정

--- app/src/main/java/com/hanyeop/happysharing/ChattingActivity.kt
+++ app/src/main/java/com/hanyeop/happysharing/ChattingActivity.kt
@@ -53,7 +53,11 @@ class ChattingActivity : AppCompatActivity() {
             // 메시지 입력 시 리사이클러뷰 크기 조절
             messageRecyclerView.addOnLayoutChangeListener {
                     view, left, top, right, bottom, oldLeft, oldRight, oldTop, oldBottom ->
-                if (bottom > oldBottom && ::chatAdapter.isInitialized){
+//                if (bottom > oldBottom && ::chatAdapter.isInitialized){
+//                    messageRecyclerView.scrollToPosition(chatAdapter.itemCount-1)
+//                }
+
+                if(::chatAdapter.isInitialized){
                     messageRecyclerView.scrollToPosition(chatAdapter.itemCount-1)
                 }
             }
