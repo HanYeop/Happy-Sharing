dca4a273d179459008cd8d52bbcb5cf316a331a2
Hanyeop <super7615@naver.com>
2021-09-22 16:33:45 +0900

    랭킹 구현

---
+++ app/src/main/java/com/hanyeop/happysharing/adapter/RankingAdapter.kt
@@ -0,0 +1,77 @@
+package com.hanyeop.happysharing.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.hanyeop.happysharing.R
+import com.hanyeop.happysharing.databinding.ItemRankingBinding
+import com.hanyeop.happysharing.model.ItemDTO
+import com.hanyeop.happysharing.model.UserDTO
+import com.hanyeop.happysharing.util.Constants.Companion.TAG
+import com.hanyeop.happysharing.util.Utility
+
+class RankingAdapter : RecyclerView.Adapter<RankingAdapter.RankingViewHolder>() {
+
+    private var userList: ArrayList<UserDTO> = arrayListOf()
+
+    // Firestore 초기화
+    private val fireStore = FirebaseFirestore.getInstance()
+
+    init {
+        // 유저 정보 불러옴
+        fireStore.collection("users").orderBy("score",Query.Direction.DESCENDING)
+            .limit(5).get()
+            .addOnCompleteListener { documentSnapshot ->
+
+                if (documentSnapshot.isSuccessful) {
+                    // 리스트 불러오기
+                    for(snapshot in documentSnapshot.result){
+                        var user = snapshot.toObject(UserDTO::class.java)
+                        userList.add(user)
+                    }
+                }
+                notifyDataSetChanged()
+            }
+    }
+
+    inner class RankingViewHolder(private val binding: ItemRankingBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        // 유저 정보 표시
+        fun bind(userDTO: UserDTO,position: Int){
+            binding.apply {
+                userIdText.text = userDTO.userId.toString()
+                scoreNumberText.text = userDTO.score.toString()
+                shareNumberText.text = userDTO.sharing.toString()
+                rankNumText.text = (position+1).toString()
+                areaText2.text = userDTO.area.toString()
+
+                Glide.with(profileImageView.context)
+                    .load(userDTO.imageUri)
+                    .placeholder(R.drawable.ic_baseline_person_24)
+                    .apply(RequestOptions().circleCrop()).into(profileImageView)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RankingAdapter.RankingViewHolder {
+        val binding = ItemRankingBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return RankingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RankingAdapter.RankingViewHolder, position: Int) {
+        holder.bind(userList[position],position)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
--- app/src/main/java/com/hanyeop/happysharing/fragment/RankingFragment.kt
+++ app/src/main/java/com/hanyeop/happysharing/fragment/RankingFragment.kt
@@ -1,8 +1,43 @@
 package com.hanyeop.happysharing.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.hanyeop.happysharing.R
+import com.hanyeop.happysharing.adapter.RankingAdapter
+import com.hanyeop.happysharing.databinding.FragmentMoreBinding
+import com.hanyeop.happysharing.databinding.FragmentRankingBinding
 
 class RankingFragment : Fragment(R.layout.fragment_ranking) {
+
+    // 참조 관리
+    private var _binding : FragmentRankingBinding? = null
+    private val binding get() = _binding!!
+
+    // rankingAdapter 선언언
+   private lateinit var rankingAdapter: RankingAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 뷰바인딩
+        _binding = FragmentRankingBinding.bind(view)
+
+        // 툴바 텍스트 변경
+        val toolbar : androidx.appcompat.widget.Toolbar = requireActivity().findViewById(R.id.toolbar)
+        toolbar.title = "점수 랭킹"
+
+        binding.apply {
+            rankingAdapter = RankingAdapter()
+            rankingRecyclerView.adapter = rankingAdapter
+        }
+    }
+
+
+    // 프래그먼트는 뷰보다 오래 지속 . 프래그먼트의 onDestroyView() 메서드에서 결합 클래스 인스턴스 참조를 정리
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
